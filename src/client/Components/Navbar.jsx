2825db6b1088366b023c4011dcccc76669c8af33
Corey Dabrowski <143559802+CoreyD24@users.noreply.github.com>
2024-01-24 09:29:47 -0700

    Update Navbar.jsx

--- src/client/Components/Navbar.jsx
+++ src/client/Components/Navbar.jsx
@@ -1,31 +1,55 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/blissful_journey_logo.jpeg"
-
+import { useState } from 'react';
 
 const Navbar = ({ token, isAdmin }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <>
-    <nav
-    class="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4 .sticky .top-0">
-    <div class="flex w-full flex-wrap items-center justify-between px-3">
-      <div>
-        <img src={logo} alt="Blissful Journey Logo"/>
-        <Link to="/">Home</Link>
+    <nav class="bg-white">
+      <div class="container mx-auto px-3">
+        <div class="flex justify-between">
+          <div class="flex space-x-4">
+            <Link to="/" class="flex items-center py-4 px-2 text-gray-700 hover:text-gray-900">
+              <img src={logo} alt="Blissful Journey Logo" class="h-10 w-10 mr-3 md:h-20 md:w-20" />
+              <span class="hidden md:inline">Home</span>
+            </Link>
+            <div class="hidden md:flex items-center space-x-1">
+              <Link class="py-4 px-2 text-gray-700  hover:text-gray-900 " to="/destinations">Destinations</Link>
+              <Link class="py-4 px-2 text-gray-700  hover:text-gray-900" to="/interests">Browse By Interest</Link>
+              <Link class="py-4 px-2 text-gray-700  hover:text-gray-900" to="/quiz">Take Our Vacation Quiz</Link>
+            </div>
+          </div>
+          <div class="hidden md:flex items-center space-x-1 ">
+            {token ? (<Link class="py-4 px-2 text-gray-700  hover:text-gray-900" to="/account">Account</Link>) : (
+              <Link class="py-4 px-2 text-gray-700  hover:text-gray-900" to="/login">Login</Link>)}
+            {!token ? <Link class="py-2 px-2 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300" to="/register">Register</Link> : ""}
+            {token ? (<Link class="py-4 px-2  text-gray-700  hover:text-gray-900" to="/wishlist">My Wishlist</Link>) : ""}
+          </div>
+        </div>
+        <div class="md:hidden flex items-center">
+          <button class="mobile-menu-button" onClick={toggleMenu}>
+            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+        </div>
+        <div className={`mobile-menu ${isMenuOpen ? '' : 'hidden'} md:hidden`}>
+          <Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/">Home</Link>
+          <Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/destinations">Destinations</Link>
+          <Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/interests">Browse By Interest</Link>
+          <Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/quiz">Take Our Vacation Quiz</Link>
+          {token ? (<Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/account">Account</Link>) : (
+            <Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/login">Login</Link>)}
+          {!token ? <Link class="block py-2 px-4 text-sm  hover:bg-gray-200" to="/register">Register</Link> : ""}
+          {token ? (<Link class="block py-2 px-4 text-sm hover:bg-gray-200" to="/wishlist">My Wishlist</Link>) : ""}
+        </div>
       </div>
-      <div>
-        <Link to="/destinations">Destinations</Link>
-        <Link to="/interests">Browse By Interest</Link>
-        {/* 
-        <Link to="/quiz">Take Our Vacation Quiz</Link>
-        search bar */}
-        {token ? (<Link to="/account">Account</Link>) : (
-          <Link to="/login">Login</Link>)}
-        {!token ? <Link to="/register">Register</Link>: ""}
-        {token ? (<Link to="/wishlist">My Wishlist</Link>) : ""}
-      </div>
-    </div>
     </nav>
-    </>
   );
 };
 export default Navbar;
