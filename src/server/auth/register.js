cedd8c95a9a824c4e55c6cbb9f50b3faa2ea11d3
Mark Glasman <glasman.mark@gmail.com>
2024-01-29 16:05:26 -0500

    changed register.jsx to be more in line with current security measures

--- src/server/auth/register.js
+++ src/server/auth/register.js
@@ -7,30 +7,16 @@ const { createUser, checkUserExistance } = require("../../../prisma/users");
 
 router.post("/", async (req, res, next) => {
   const { email, username, password, firstName, lastName } = req.body;
-
-  const existingUser = await checkUserExistance(username, email)
-  
+  const existingUser = await checkUserExistance(username, email);
   if (existingUser) {
     return res.status(400).json({ error: "User already exists" });
   }
-
   const SALT_ROUNDS = 5;
   const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
   try {
-    const user = await createUser(
-      email,
-      username,
-      hashedPassword,
-      firstName,
-      lastName
-    );
-
-    const token = jwt.sign(
-      { id: user.id, username: user.username, isAdmin: user.isAdmin },
-      process.env.JWT_SECRET
-    )
-    res.status(201).send({ token })
-
+    const user = await createUser(email, username, hashedPassword, firstName, lastName);
+    const token = jwt.sign({ id: user.id, username: user.username, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
+    res.status(201).send({ message: "Registration Successful", token, admin: user.isAdmin, id: user.id });
   } catch (error) {
     console.error(error);
   }
