2c6466ebd3eb6817df72f002edb7b0a08bcdd85e
Lucas Woods <lukewoods@Lucass-MacBook-Pro.local>
2024-02-01 13:33:08 -0600

    completed styling for account page, accidentally forgot to change branch name

--- src/client/Components/Account.jsx
+++ src/client/Components/Account.jsx
@@ -23,35 +23,37 @@ const Account = ({ setToken, setIsAdmin, setUserId }) => {
   };
 
   const updateUserHandle = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const userId = window.localStorage.getItem("Id");
     try {
-      const response = await axios.patch(`/api/users/${userId}`,{
-        data:{
+      const response = await axios.patch(`/api/users/${userId}`, {
+        data: {
           username,
           firstName,
           lastName,
-          email
-        }
-      })
-    window.alert("Account Updated")
-    setUsername("");
-    setFirstName("")
-    setLastName("")
-    setEmail("")
-    setShowUpdateForm(false)
-    setUser(response.data)
+          email,
+        },
+      });
+      window.alert("Account Updated");
+      setUsername("");
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setShowUpdateForm(false);
+      setUser(response.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleDelete = async (userId, interestId) => {
     try {
       const response = await axios.delete(`/api/users_interests`, {
         data: { userId, interestId },
       });
-      setInterests((prevInterests) => prevInterests.filter((int) => int.interestId !== interestId));
+      setInterests((prevInterests) =>
+        prevInterests.filter((int) => int.interestId !== interestId)
+      );
     } catch (e) {
       console.error(e);
     }
@@ -81,56 +83,106 @@ const Account = ({ setToken, setIsAdmin, setUserId }) => {
   }, []);
 
   return (
-    <>
-      <h1>
-        Name: {user.firstName} {user.lastName}
-      </h1>
-      <h1>Email: {user.email}</h1>
-      <h1>Username: {user.username}</h1>
-      <ul>
-        My Interests
-        <div>
-          <br />
-          {interests?.map((int) => (
-            <div key={int.interestId}>
-              <h2>{int.Interest.name}</h2>
-              <h3>{int.Interest.description}</h3>
-              <button className="delete-button" onClick={() => handleDelete(user.id, int.interestId)}>
-                X
-              </button>
-              <br />
-            </div>
-          ))}
-        </div>
-      </ul>
-      <button onClick={logOut}>Log Out</button>
-      <br/>
-      <button onClick={() => setShowUpdateForm(true)}>Update Profile</button>
-          {showUpdateForm && (
-              <form>
-                <label>
-                  Username
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-                </label>
-                <label>
-                  First Name
-                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                </label>
-                <label>
-                  Last Name
-                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-                </label>
-                <label>
-                  Email
-                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                </label>
-            <br/>
-            <button onClick={() => setShowUpdateForm(false)}>Cancel</button>
-            <br/>
-            <button onClick={updateUserHandle}>Update</button>
-              </form>
-          )}
-    </>
+    <div className="md:h-full flex items-center text-gray-600">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl md:text-5xl text-gray-700 font-semibold mb-3">
+          Name: {user.firstName} {user.lastName}
+        </h1>
+        <h1 className="text-4xl md:text-5xl text-gray-700 font-semibold mb-3">
+          Email: {user.email}
+        </h1>
+        <h1 className="text-4xl md:text-5xl text-gray-700 font-semibold mb-3">
+          Username: {user.username}
+        </h1>
+        <ul className="text-2xl">
+          My Interests
+          <div className="mb-4">
+            <br />
+            {interests?.map((int) => (
+              <div key={int.interestId} className="mb-4">
+                <h2 className="text-3xl">{int.Interest.name}</h2>
+                <h3 className="text-lg">{int.Interest.description}</h3>
+                <button
+                  className="text-lg text-red-500"
+                  onClick={() => handleDelete(user.id, int.interestId)}
+                >
+                  X
+                </button>
+                <br />
+              </div>
+            ))}
+          </div>
+        </ul>
+        <button
+          onClick={logOut}
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Log Out
+        </button>
+        <br />
+        <button
+          onClick={() => setShowUpdateForm(true)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        >
+          Update Profile
+        </button>
+        {showUpdateForm && (
+          <form className="mt-4">
+            <label className="block text-lg mb-2">
+              Username
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:ring focus:border-blue-500"
+              />
+            </label>
+            <label className="block text-lg mb-2">
+              First Name
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:ring focus:border-blue-500"
+              />
+            </label>
+            <label className="block text-lg mb-2">
+              Last Name
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:ring focus:border-blue-500"
+              />
+            </label>
+            <label className="block text-lg mb-2">
+              Email
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="border-2 border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:ring focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <button
+              onClick={() => setShowUpdateForm(false)}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Cancel
+            </button>
+            <br />
+            <button
+              onClick={updateUserHandle}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            >
+              Update
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
   );
 };
+
 export default Account;
