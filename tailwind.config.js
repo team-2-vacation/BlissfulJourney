16a5e243c33f0eaa66f96ed9aa6e0ed2df9c3c06
Mark Glasman <glasman.mark@gmail.com>
2024-02-01 13:03:44 -0500

    bit more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'custom': ['Roboto', 'sans-serif'], 
+        'custom': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        blissfulBlue: {
+          '100': '#0062e2',
+          '200': '#0467ab',
+          '300': '#071c2d',
+        },
       },
     },
   },
   plugins: [],
-}
+};
